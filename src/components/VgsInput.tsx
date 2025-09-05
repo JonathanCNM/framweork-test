e2d56292963971f19f95cb5815512ffa035f4736
JonathanCNM <jonathan.camilo@leapfinancial.com>
2025-09-05 15:52:41 -0500

    fixed VGS Input behavior

--- src/components/VgsInput.tsx
+++ src/components/VgsInput.tsx
@@ -49,7 +49,7 @@ export const VgsInput: React.FC<VgsInputProps> = ({
   const styles = {
     "--radius": `${borderRadius}px`,
     "--bg":
-      errorLabel && !isValid
+      !isValid && isFocus
         ? errorColor
         : isFocus
         ? activeColor
@@ -59,7 +59,7 @@ export const VgsInput: React.FC<VgsInputProps> = ({
   };
 
   const labelColors =
-    errorLabel && !isValid
+    !isValid && isFocus
       ? errorColor
       : isFocus
       ? activeColor

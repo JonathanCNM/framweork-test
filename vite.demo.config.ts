9bf434adafd23217bf41ba98a16f77991d5fdb86
JonathanCNM <jonathan.camilo@leapfinancial.com>
2025-10-30 15:05:07 -0500

    fixing build demo script

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../demo-dist",
+    outDir: "demo-dist",
     emptyOutDir: true,
   },
   server: {
